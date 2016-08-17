1aaaa79ae8858ceb8ae7ce08b4ca3ffad8c1dc4c
Kyle Conroy <kyle@kyleconroy.com>
2016-08-16 23:14:28 -0700

    Move everything to 1.0.0-pre2

--- 0.11/csharp/README.md
+++
@@ -1,4 +0,0 @@
-# gRPC C# Docker image
-
-This is the official docker image for the C# installation of gRPC. 
-
--- 0.11/php/sample_app/client.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-require 'helloworld.php';
-
-$client = new helloworld\GreeterClient('localhost:50051', []);
-var_dump($client);
